882edb5f02d535a544865bcff89ff64e66fc44b6
iosuser11 <matchkalyananahit@gmail.com>
2016-08-31 17:04:38 +0400

    a

--- app/src/main/java/com/example/iosuser11/postonwall/GRVCoordinates.java
+++ app/src/main/java/com/example/iosuser11/postonwall/GRVCoordinates.java
@@ -7,16 +7,14 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 
-public class GRVCoordinates implements SensorEventListener
-{
+public class GRVCoordinates implements SensorEventListener {
     SensorManager mSensorManager;
     private Sensor mSensor;
 
     //[0] = x , [1] = y [2] = z
     private float[] values = new float[3];
 
-    public GRVCoordinates(Activity activity)
-    {
+    public GRVCoordinates(Activity activity) {
         // Get an instance of the SensorManager
         mSensorManager = (SensorManager) activity.getSystemService(activity.SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GAME_ROTATION_VECTOR);
@@ -24,33 +22,28 @@ public class GRVCoordinates implements SensorEventListener
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
-    public float[] getValues()
-    {
-        return values;
-    }
-
-    public float[] getRotationMatrix()
-    {
+    public float[] getRotationMatrix() {
         float[] mat = new float[16];
         mSensorManager.getRotationMatrixFromVector(mat, values);
         return mat;
     }
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent)
-    {
-        if (sensorEvent.sensor.getType() != Sensor.TYPE_GAME_ROTATION_VECTOR)
-        {
-            return;
-        } else
-        {
+    public void onSensorChanged(SensorEvent sensorEvent) {
+//        if (sensorEvent.sensor.getType() != Sensor.TYPE_GAME_ROTATION_VECTOR) {
+//            return;
+//        } else {
+//            values = sensorEvent.values.clone();
+//        }
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_GAME_ROTATION_VECTOR) {
             values = sensorEvent.values.clone();
+        } else if(sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+        } else {
+            return;
         }
     }
 
     @Override
-    public void onAccuracyChanged(Sensor sensor, int i)
-    {
-
-    }
+    public void onAccuracyChanged(Sensor sensor, int i) {}
 }
